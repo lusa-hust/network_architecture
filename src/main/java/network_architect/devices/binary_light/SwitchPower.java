3151efe0ecf39052d42e5abc7667af4b405ce6ee
LuongNV <luongnv@ows.edu.vn>
2017-11-28 23:07:33 +0700

    Add: GUI for service

--- src/main/java/network_architect/devices/binary_light/SwitchPower.java
+++ src/main/java/network_architect/devices/binary_light/SwitchPower.java
@@ -2,6 +2,9 @@ package network_architect.devices.binary_light;
 
 import org.fourthline.cling.binding.annotations.*;
 import java.beans.PropertyChangeSupport;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 // binding annotation
 @UpnpService(
@@ -11,9 +14,15 @@ import java.beans.PropertyChangeSupport;
 public class SwitchPower {
 
     private final PropertyChangeSupport propertyChangeSupport;
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
 
     public SwitchPower() {
         this.propertyChangeSupport = new PropertyChangeSupport(this);
+        this.prepareGUI();
+        this.showEventDemo();
     }
 
     public PropertyChangeSupport getPropertyChangeSupport() {
@@ -52,6 +61,68 @@ public class SwitchPower {
         // If you want to pass extra UPnP information on error:
         // throw new ActionException(ErrorCode.ACTION_NOT_AUTHORIZED);
         return status;
+    }
+
+    private void prepareGUI(){
+        mainFrame = new JFrame("Java SWING Examples");
+        mainFrame.setSize(400,400);
+        mainFrame.setLayout(new GridLayout(3, 1));
+
+        headerLabel = new JLabel("",JLabel.CENTER );
+        statusLabel = new JLabel("",JLabel.CENTER);
+        statusLabel.setSize(350,100);
+
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+
+        mainFrame.add(headerLabel);
+        mainFrame.add(controlPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
+        System.out.println("Run");
+    }
+
+    private void showEventDemo(){
+        headerLabel.setText("Control in action: Button");
+
+        JButton okButton = new JButton("OK");
+        JButton submitButton = new JButton("Submit");
+        JButton cancelButton = new JButton("Cancel");
+
+        okButton.setActionCommand("OK");
+        submitButton.setActionCommand("Submit");
+        cancelButton.setActionCommand("Cancel");
+
+        okButton.addActionListener(new ButtonClickListener());
+        submitButton.addActionListener(new ButtonClickListener());
+        cancelButton.addActionListener(new ButtonClickListener());
+
+        controlPanel.add(okButton);
+        controlPanel.add(submitButton);
+        controlPanel.add(cancelButton);
+
+        mainFrame.setVisible(true);
+    }
+
+    private class ButtonClickListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+
+            if( command.equals( "OK" ))  {
+                statusLabel.setText("Ok Button clicked.");
+                setTarget(true);
+            } else if( command.equals( "Submit" ) )  {
+                statusLabel.setText("Submit Button clicked.");
+            } else {
+                statusLabel.setText("Cancel Button clicked.");
+                setTarget(false);
+            }
+        }
     }
 
 }
